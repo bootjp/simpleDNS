22b59b215fb7282c1c54140a42c3379b9ce7b786
bootjp / Yoshiaki Ueda <contact@bootjp.me>
2021-10-01 22:05:25 +0900

    implements cache

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 
 	"github.com/google/gopacket/layers"
 
@@ -12,10 +13,18 @@ import (
 )
 
 var logger *log.Logger
+var cache map[string]AnswerCache
+
+type AnswerCache struct {
+	Response  layers.DNS
+	SolveTime time.Time
+}
 
 func init() {
-	l := log.New(os.Stdout, "[Hello] ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+	l := log.New(os.Stdout, "[simpleDNS] ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 	logger = l
+
+	cache = map[string]AnswerCache{}
 }
 
 func main() {
@@ -33,9 +42,10 @@ func main() {
 			logger.Println(err)
 			continue
 		}
+
 		p := gopacket.NewPacket(buffer[:length], layers.LayerTypeDNS, gopacket.Default)
 		if p.ErrorLayer() != nil {
-			//logger.Println(p.ErrorLayer().Error())
+			logger.Println(p.ErrorLayer().Error())
 			continue
 		}
 
@@ -46,27 +56,67 @@ func main() {
 			continue
 		}
 
-		fmt.Println("request ")
-		fmt.Println(dns.Questions[0].Type.String() + " " + string(dns.Questions[0].Name) + " " + dns.Questions[0].Class.String())
+		name := string(dns.Questions[0].Name)
+		cn, ok := cache[name]
 
-		answer := &layers.DNS{ID: dns.ID, QR: true, OpCode: layers.DNSOpCodeQuery, AA: true, RD: true, RA: true}
+		if ok {
+
+			sub := time.Now().Unix() - cn.SolveTime.Unix()
+			fmt.Println("sub", sub, cn.Response.Answers[0].TTL, sub >= int64(cn.Response.Answers[0].TTL))
+			if sub >= int64(cn.Response.Answers[0].TTL) {
+				logger.Println("stale cache")
+				delete(cache, name)
+				logger.Println("purged " + name)
+				goto res
+			}
+			cn.Response.ID = dns.ID
+			logger.Println("use cache")
+			buf := gopacket.NewSerializeBuffer()
+			opts := gopacket.SerializeOptions{FixLengths: true}
+			err = gopacket.SerializeLayers(buf, opts, &cn.Response)
+			if err != nil {
+				logger.Println(err)
+			}
+
+			if _, err := conn.WriteTo(buf.Bytes(), addr); err != nil {
+				logger.Println(err)
+			}
+			continue
+		}
+	res:
+
+		logger.Println("request " + dns.Questions[0].Type.String() + " " + string(dns.Questions[0].Name) + " " + dns.Questions[0].Class.String())
+
+		answer := &layers.DNS{
+			ID:     dns.ID,
+			QR:     true,
+			OpCode: layers.DNSOpCodeQuery,
+			AA:     true,
+			RD:     true,
+			RA:     true,
+		}
 		answer.Answers = append(dns.Answers,
 			layers.DNSResourceRecord{
 				Name:  []byte("bootjp.me"),
-				Type:  layers.DNSTypeCNAME,
+				Type:  layers.DNSTypeA,
 				Class: layers.DNSClassIN,
-				TTL:   1024,
-				CNAME: []byte("twitter.com/bootjp"),
+				TTL:   10,
+				IP:    net.IPv4(192, 168, 0, 1),
 			})
 
 		buf := gopacket.NewSerializeBuffer()
 		opts := gopacket.SerializeOptions{FixLengths: true}
 		err = gopacket.SerializeLayers(buf, opts, answer)
 		if err != nil {
-			logger.Fatal(err)
-
+			logger.Println(err)
 		}
 
-		conn.WriteTo(buf.Bytes(), addr)
+		if _, err := conn.WriteTo(buf.Bytes(), addr); err != nil {
+			logger.Println(err)
+		}
+		cache[name] = AnswerCache{
+			SolveTime: time.Now(),
+			Response:  *answer,
+		}
 	}
 }
